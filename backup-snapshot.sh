a75fa4a831ad4007b58d2a49807cca66f7a3ef2c
Sagar Gokhale <sagar.gokhale@gmail.com>
2015-08-31 17:52:29 -0700

    Both tested and working.

--- backup-snapshot.sh
+++ backup-snapshot.sh
@@ -5,19 +5,18 @@ if [ "$volume_group_path" = "" ]; then
     echo "must give volume_group_path where snapshot exists"
     exit 1;
 fi;
-lvdisplay=``
 
 snapshot_volume_path=`lvdisplay "$volume_group_path" | sed -n -e"s/^.*LV\ Path\s*\([0-9a-zA-Z/\-]\+-snapshot\)$/\1/pg" | tail -1`
-lvdisplay=`lvdisplay "$snapshot_volume_path"`
-snapshot_volume_name=`echo $lvdisplay | sed -n -e"s/^.*LV\ Name\s*\([0-9a-zA-Z/\-]\+-snapshot\)$/\1/pg"`
-volume_open=`echo $lvdisplay | sed -n -e "s/^.*# open\s*\([0-9]\+\)$/\1/pg"`
+snapshot_volume_name=`lvdisplay "$snapshot_volume_path" | sed -n -e"s/^.*LV\ Name\s*\([0-9a-zA-Z/\-]\+-snapshot\)$/\1/pg"`
+volume_open=`lvdisplay "$snapshot_volume_path" | sed -n -e "s/^.*# open\s*\([0-9]\+\)$/\1/pg"`
 
 if [ "$volume_open" != "0" ]; then
     echo "$snapshot_volume_path is already open. Please close it first."
     exit 1;
 fi;
 
-snap_used=`df /snapshot --output="used" | sed -n -e "s/^\/snapshot\s\+\([0-9]\+\)$/\1/gp"`
+snap_used=`df /snapshot --output="target,used" | sed -n -e "s/^\/snapshot\s\+\([0-9]\+\)$/\1/gp"`
+
 if [ "$snap_used" != "" ]; then
     echo "Something is mounted at /snapshot."
     exit 1;
@@ -25,7 +24,7 @@ fi;
 
 mount -o ro "$snapshot_volume_path" /snapshot;
 
-snap_used=`df /snapshot --output="used" | sed -n -e "s/^\/snapshot\s\+\([0-9]\+\)$/\1/gp"`
+snap_used=`df /snapshot --output="target,used" | sed -n -e "s/^\/snapshot\s\+\([0-9]\+\)$/\1/gp"`
 if [ "$snap_used" = "" ]; then
     echo "could not mount /snapshot... something went wrong."
     exit 1;
@@ -33,8 +32,7 @@ fi;
 
 # /backup must exist as a separate mount point and must have enough
 # disk space.
-
-free_blocks=`df /backup --output="avail" | sed -n -e "s/^\/backup\s\+\([0-9]\+\)$/\1/gp"`
+free_blocks=`df /backup --output="target,avail" | sed -n -e "s/^\/backup\s\+\([0-9]\+\)$/\1/gp"`
 
 if [ "$free_blocks" = "" -a $free_blocks -gt $snap_used ]; then
     echo "/backup must be a different volume with enough disk space!"
@@ -44,9 +42,11 @@ fi;
 
 d=`date +"%Y_%m_%d-%H_%M_%S"`
 backup_name="/backup/${snapshot_volume_name}-${d}.tar.gz"
-tar -cpzvf "$backup_name" /snapshot
+cd /snapshot
+tar -cpzvf "$backup_name" *
 sha256sum "$backup_name" > "${backup_name}.sha256sum"
 
+cd -
 umount /snapshot
 
 echo "about to execute: lvremove \"$snapshot_volume_path\""
--- snapshot-volume.sh
+++ snapshot-volume.sh
@@ -10,7 +10,7 @@ if [ ! -e "$volume_path" ]; then
     exit 1;
 fi;
 
-volume_name=`lvdisplay "$volume_path"  | grep "LV Name" | sed -n -e"s/^.*LV\ Name\s*\([A-Za-z0-9\-]*\)$/\1/pg"`
+volume_name=`lvdisplay "$volume_path"  | sed -n -e"s/^.*LV\ Name\s*\([A-Za-z0-9\-]*\)$/\1/pg"`
 vg_path=`echo "$volume_path" | sed -n -e "s/\(.*\)\/[a-zA-Z0-9\-]*/\1/pg"`
 
 active_snapshot=`lvdisplay | grep "LV snapshot status" | sed -n -e"s/.*active\ destination\ for\ \(.*\)$/\1/pg"`
@@ -24,4 +24,5 @@ snapshot_volume_name="${volume_name}-snapshot"
 # I should try to calculate the size of the snapshot volume, but for now,
 # we always create a 10G snapshot. 
 # If left around, this might fill up!
+
 /sbin/lvcreate -L10G -s -n "${snapshot_volume_name}" "$volume_path"
